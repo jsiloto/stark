30ba0263666e08dccc92b6f590ef8d1f154f66a9
Juliano Siloto <julianosiloto@gmail.com>
2025-06-15 17:27:24 -0300

    Add webhook app, Working Integration, failing logic

--- webhook/webhooks.py
+++ webhook/webhooks.py
@@ -2,29 +2,87 @@ from flask import Flask, request
 import uuid
 import json
 import os
+import starkbank
+from starkbank import Transfer
+
+private_key_content = """
+-----BEGIN EC PARAMETERS-----
+BgUrgQQACg==
+-----END EC PARAMETERS-----
+-----BEGIN EC PRIVATE KEY-----
+MHQCAQEEIHX3joDOT1x+Wgf9JKh+UykiuBFSOnNk93Z2p1WhfOHXoAcGBSuBBAAK
+oUQDQgAE9yvqkZBi2bp+y8JUYolrS1OVCJ94ICrrvfBJl+vavZBSeZ0dLkqu5zMW
+3JzTFaGqN9MSfydn3RnMqkPSYrdhzA==
+-----END EC PRIVATE KEY-----
+"""
+
+
 
 app = Flask(__name__)
 
 SAVE_DIR = '../webhook_data'
 os.makedirs(SAVE_DIR, exist_ok=True)
 
+class TransferCredits:
+    def __init__(self):
+        self.project = self.start_project()
+
+    def start_project(self):
+        project = starkbank.Project(
+            environment="sandbox",
+            id="6028207048359936",
+            private_key=private_key_content
+        )
+
+        starkbank.user = project
+        return project
+
+    def generate_transfer(self, invoice: object):
+        transfer = starkbank.Transfer(
+                amount=invoice['amount'],
+                bank_code="20018183",  # TED
+                branch_code="0001",
+                account_number="6341320293482496",
+                account_type="payment",
+                tax_id="20.018.183/0001-80",
+                name="Stark Bank S.A."
+            )
+        starkbank.transfer.create([transfer])
 
 
+transfer_credits = TransferCredits()
+@app.route('/invoice', methods=['POST'])
+def invoice():
+    data = request.json['event']
+    if "log" not in data:
+        return 'Invalid event data', 400
 
-@app.route('/webhook', methods=['POST'])
-def webhook():
-    data = request.json
-    print("Received webhook:", data)
+    if "invoice" not in data['log']:
+        return 'No invoice data found', 400
 
-    # Create a unique filename using UUID
-    filename = f"{uuid.uuid4()}.json"
-    filepath = os.path.join(SAVE_DIR, filename)
-
-    # Save the data to a file
-    with open(filepath, 'w') as f:
-        json.dump(data, f, indent=2)
+    invoice = data['log']['invoice']
+    print(f"Received webhook: {invoice['id']}, Amount: {invoice['amount']}")
+    transfer_credits.generate_transfer(invoice)
 
     return '', 200
+
+@app.route('/transfer', methods=['POST'])
+def transfer():
+    data = request.json['event']
+    if "log" not in data:
+        return 'Invalid event data', 400
+
+    if "transfer" not in data['log']:
+        return 'No transfer data found', 400
+
+    transfer = data['log']['transfer']
+    print(f"Received webhook: {transfer}")
+
+    return '', 200
+
+
+
+
 
 if __name__ == '__main__':
     app.run(port=5000)
