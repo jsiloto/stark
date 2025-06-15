04993a08e2d6e9e592553eefa1babe2b50ffc153
Juliano Siloto <julianosiloto@gmail.com>
2025-06-15 17:01:59 -0300

    Make Sender App

--- invoice/invoice.py
+++ invoice/invoice.py
@@ -1,10 +1,13 @@
+import time
+
 import starkbank
 # coding: utf-8
 import starkbank
-from datetime import date, datetime, timedelta
+from datetime import date, datetime, timedelta, UTC
+import random
+import faker
+import argparse
 
-# Get your private key from an environment variable or an encrypted database.
-# This is only an example of a private key content. You should use your own key.
 private_key_content = """
 -----BEGIN EC PARAMETERS-----
 BgUrgQQACg==
@@ -16,47 +19,85 @@ oUQDQgAE9yvqkZBi2bp+y8JUYolrS1OVCJ94ICrrvfBJl+vavZBSeZ0dLkqu5zMW
 -----END EC PRIVATE KEY-----
 """
 
-project = starkbank.Project(
-    environment="sandbox",
-    id="6028207048359936",
-    private_key=private_key_content
-)
 
-starkbank.user = project
+def generate_random_cpf():
+    def calculate_digit(numbers):
+        s = sum([int(digit) * weight for digit, weight in zip(numbers, range(len(numbers) + 1, 1, -1))])
+        d = 11 - s % 11
+        return str(d if d < 10 else 0)
 
-from datetime import datetime, timedelta, UTC
+    numbers = [str(random.randint(0, 9)) for _ in range(9)]
+    numbers.append(calculate_digit(numbers))
+    numbers.append(calculate_digit(numbers))
+    return "{}{}{}.{}{}{}.{}{}{}-{}{}".format(*numbers)
 
 
-invoices = starkbank.invoice.create([
-    starkbank.Invoice(
-        amount=23571,  # R$ 235,71
-        name="Buzz Aldrin",
-        tax_id="012.345.678-90",
-        due=datetime.now(UTC) + timedelta(hours=1),
-        expiration=timedelta(hours=3).total_seconds(),
-        fine=5,  # 5%
-        interest=2.5,  # 2.5% per month
-        tags=["immediate"],
+class InvoiceSender:
+    def __init__(self):
+        self.project = self.start_project()
+        self.database = self.generate_database()
 
-    ),
-    starkbank.Invoice(
-        amount=23571,  # R$ 235,71
-        name="Buzz Aldrin",
-        tax_id="012.345.678-90",
-        due=date(2025, 7, 20),
-        expiration=timedelta(hours=3).total_seconds(),
-        fine=5,  # 5%
-        interest=2.5,  # 2.5% per month
-        tags=["scheduled"]
-    )
-])
+    def start_project(self):
+        project = starkbank.Project(
+            environment="sandbox",
+            id="6028207048359936",
+            private_key=private_key_content
+        )
 
-for invoice in invoices:
-    print(invoice)
+        starkbank.user = project
+        return project
 
-import time
-import requests
-from datetime import datetime
+
+    def generate_database(self):
+        db = []
+        fake = faker.Faker()
+        for i in range(1000):
+            fakename = fake.name()
+            random_tax_id = generate_random_cpf()
+            db.append([fakename, random_tax_id])
+
+        return db
+
+    def generate_random_invoice(self):
+        fakename, tax_id = random.choice(self.database)
+        invoice = starkbank.Invoice(
+            amount=23571,  # R$ 235,71
+            name=fakename,
+            tax_id=tax_id,
+            due=datetime.now(UTC) + timedelta(hours=1),
+            expiration=timedelta(hours=3).total_seconds(),
+            fine=5,  # 5%
+            interest=2.5,  # 2.5% per month
+            tags=["immediate"],
+        )
+        return invoice
+
+
+    def send_invoices(self):
+        invoices = []
+        for _ in range(random.randint(8, 12)):
+            invoices.append(self.generate_random_invoice())
+        starkbank.invoice.create(invoices)
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Send invoices")
+    parser.add_argument("--recurrent", action="store_true", help="Run in recurrent mode")
+    args = parser.parse_args()
+
+    sender = InvoiceSender()
+    if args.recurrent:
+        print("Recurrent mode selected")
+        while True:
+            sender.send_invoices()
+            time.sleep(3 * 60 * 60)  # wait 3 hours
+        #
+    else:
+        sender.send_invoices()
+
+
+if __name__ == "__main__":
+    main()
 
 # WEBHOOK_URL = "http://receiver:5000/webhook"
 #
